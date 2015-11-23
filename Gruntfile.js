fc2650513d953a79565a8ffccf508e499345cf18
jansch <j.schulte@52north.org>
2015-11-23 16:20:02 +0100

    add used scripts and stylesheets by grunt build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,9 +28,7 @@ module.exports = function (grunt) {
             'www/bower_components/qr-js/qr.min.js',
             'www/bower_components/angular-leaflet-directive/dist/angular-leaflet-directive.js',
             'www/bower_components/smalot-bootstrap-datetimepicker/js/bootstrap-datetimepicker.js',
-            'www/bower_components/ng-table/dist/ng-table.js',
-            'www/bower_components/n52-sensorweb-client-core/dist/*.min.js',
-            'www/bower_components/n52-sensorweb-client-core/src/js/flotlib/*.js'
+            'www/bower_components/ng-table/dist/ng-table.js'
         ],
         lib_ie9_scripts: [
             'www/bower_components/n52-sensorweb-client-core/dist/IE9/*.min.js'
@@ -45,9 +43,50 @@ module.exports = function (grunt) {
             'www/bower_components/leaflet.markercluster/dist/MarkerCluster.Default.css',
             'www/bower_components/angular-ui-notification/dist/angular-ui-notification.min.css'
         ],
+        core_scripts: [
+            'www/bower_components/n52-sensorweb-client-core/src/js/services/**/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Map/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Map/controller/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Phenomenon/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Phenomenon/controller/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Provider/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Provider/controller/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Time/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Time/controller/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Time/directives/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Legend/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Legend/controller/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Legend/directives/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Styling/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Styling/controller/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Styling/directives/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Chart/controller/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Chart/directives/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Favorite/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Favorite/controller/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Favorite/directives/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Loading/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Loading/directives/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Table/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Table/controller/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Table/directives/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Settings/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Settings/controller/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Settings/directives/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/ListSelection/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/ListSelection/controller/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/ListSelection/directives/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/helper/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/flotlib/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Menu/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/Menu/controller/*.js'
+        ],
         app_js: [
             'www/app.js',
             'www/js/*.js'
+        ],
+        app_styles: [
+            'www/css/*.css'
         ],
         copy_files: [
             'settings.json',
@@ -62,13 +101,59 @@ module.exports = function (grunt) {
                 }
             }
         },
+        tags: {
+            options: {
+                scriptTemplate: '<script src="{{ path }}" type="text/javascript"></script>',
+                linkTemplate: '<link href="{{ path }}" rel="stylesheet" type="text/css"/>'
+            },
+            build_lib_scripts: {
+                options: {
+                    openTag: '<!-- start lib script tags -->',
+                    closeTag: '<!-- end lib script tags -->'
+                },
+                src: ['<%= lib_scripts %>'],
+                dest: 'www/index.html'
+            },
+            build_client_scripts: {
+                options: {
+                    openTag: '<!-- start client script tags -->',
+                    closeTag: '<!-- end client script tags -->'
+                },
+                src: ['<%= app_js %>'],
+                dest: 'www/index.html'
+            },
+            build_core_scripts: {
+                options: {
+                    openTag: '<!-- start core script tags -->',
+                    closeTag: '<!-- end core script tags -->'
+                },
+                src: ['<%= core_scripts %>'],
+                dest: 'www/index.html'
+            },
+            build_lib_styles: {
+                options: {
+                    openTag: '<!-- start lib style tags -->',
+                    closeTag: '<!-- end lib style tags -->'
+                },
+                src: ['<%= lib_styles %>'],
+                dest: 'www/index.html'
+            },
+            build_client_styles: {
+                options: {
+                    openTag: '<!-- start client style tags -->',
+                    closeTag: '<!-- end client style tags -->'
+                },
+                src: ['<%= app_styles %>'],
+                dest: 'www/index.html'
+            }
+        },
         concat: {
             //core: {
             //    src: 'www/js/**/*.js',
             //    dest: 'dist/js/<%= name %>.js'
             //},
             libs: {
-                src: '<%= lib_scripts %>',
+                src: ['<%= lib_scripts %>', '<%= core_scripts %>'],
                 dest: 'dist/js/deps.<%= name %>.js'
             },
             app: {
@@ -192,11 +277,12 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-script-link-tags');
     grunt.loadNpmTasks('grunt-processhtml');
     grunt.loadNpmTasks('grunt-war');
 
     grunt.registerTask('test', ['jshint']);
-
+    grunt.registerTask('env-build', ['tags']);
     grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'copy', 'processhtml']);
 
     grunt.registerTask('buildWar', ['test', 'default', 'war']);
