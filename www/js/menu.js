35ba475e41074db00f463a292c3fc3dd5e98213a
jansch <j.schulte@52north.org>
2016-01-06 21:45:52 +0100

    change the map design with new zoom control and layer selector control

--- www/js/menu.js
+++ www/js/menu.js
@@ -13,16 +13,16 @@ angular.module('n52.client.menu', []).provider('Menu',
             };
         })
         .directive('n52Menu', [function () {
-            return {
-                restrict: 'E',
-                templateUrl: 'templates/menu/menu.html',
-                scope: {
-                },
-                controller: ['$scope', 'Menu', function ($scope, Menu) {
-                        $scope.menu = Menu.getItems();
-                    }]
-            };
-        }])
+                return {
+                    restrict: 'E',
+                    templateUrl: 'templates/menu/menu.html',
+                    scope: {
+                    },
+                    controller: ['$scope', 'Menu', function ($scope, Menu) {
+                            $scope.menu = Menu.getItems();
+                        }]
+                };
+            }])
         .directive('n52MenuEntry', ['$compile', '$location', function ($compile, $location) {
                 return {
                     restrict: 'E',
@@ -43,4 +43,16 @@ angular.module('n52.client.menu', []).provider('Menu',
                         $compile(elem.children())(scope);
                     }
                 };
+            }])
+        .controller('LayerControlCtrl', ['$scope', function ($scope) {
+                angular.extend($scope, {
+                    layercontrol: {
+                        icons: {
+                            uncheck: "glyphicon glyphicon-unchecked",
+                            check: "glyphicon glyphicon-check",
+                            radio: "glyphicon glyphicon-check",
+                            unradio: "glyphicon glyphicon-unchecked"
+                        }
+                    }
+                });
             }]);
