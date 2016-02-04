8a632c9ab08d0613d55ce3e8bd2545a87f62acc1
jansch <j.schulte@52north.org>
2016-02-04 10:05:29 +0100

    update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -85,7 +85,8 @@ module.exports = function (grunt) {
             'www/bower_components/n52-sensorweb-client-core/src/js/services/**/*.js',
             'www/bower_components/n52-sensorweb-client-core/src/js/helper/*.js',
             'www/bower_components/n52-sensorweb-client-core/src/js/helper/controller/*.js',
-            'www/bower_components/n52-sensorweb-client-core/src/js/helper/service/*.js'
+            'www/bower_components/n52-sensorweb-client-core/src/js/helper/service/*.js',
+            'www/bower_components/n52-sensorweb-client-core/src/js/plugins/extendedGetTsData.js'
         ],
         app_js: [
             'www/app.js',
