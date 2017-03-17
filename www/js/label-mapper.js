410758f509e76c22c96d783a9a4dd6580576f022
Jan Schulte <j.schulte@52north.org>
2017-03-17 08:15:28 +0100

    update label-mapper and use it in all templates with labels

--- www/js/label-mapper.js
+++ www/js/label-mapper.js
@@ -4,64 +4,52 @@ angular.module('n52.core.legend')
             label: '<'
         },
         templateUrl: 'n52.core.legend.label-mapper',
-        controller: ['$http', '$q',
-            function($http, $q) {
-                var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        controller: ['$http', '$q', 'labelMapperSrvc',
+            function($http, $q, labelMapperSrvc) {
                 this.$onInit = function() {
-                    var urls = findUrls(this.label);
-                    if (urls.length > 0) {
-                        var request = [];
-                        urls.forEach((url) => {
-                            request.push($http.get(proxyUrl + url, {
-                                cache: true
-                            }).then((response) => {
-                                var xml = jQuery.parseXML(response.data);
-                                this.label = this.label.replace(url, $(xml).find('prefLabel').text());
-                            }, (error) => {}));
-                        });
-                        $q.all(request).then(() => {
-                            this.determinedLabel = this.label;
-                        });
-                    } else {
-                        this.determinedLabel = this.label;
-                    }
-                };
-
-                var findUrls = function(text) {
-                    var source = (text || '').toString();
-                    var urlArray = [];
-                    var url;
-                    var matchArray;
-                    // Regular expression to find FTP, HTTP(S) and email URLs.
-                    var regexToken = /(((ftp|https?):\/\/)[\-\w@:%_\+.~#?&\/\/=]+)/g;
-                    // Iterate through any URLs in the text.
-                    while ((matchArray = regexToken.exec(source)) !== null) {
-                        var token = matchArray[0];
-                        urlArray.push(token);
-                    }
-                    return urlArray;
+                    labelMapperSrvc.getMappedLabel(this.label).then((label) => {
+                        this.determinedLabel = label;
+                    });
                 };
             }
         ]
     })
-    // .service('labelMapperSrvc', ['$q',
-    //     function($q) {
-    //         this.getMappedLabel(label) {
-    //             return $q(function(resolve, reject) {
-    //
-    //                 if (serviceRootUrlToVersionMap[apiUrl]) {
-    //
-    //                     resolve(serviceRootUrlToVersionMap[apiUrl]);
-    //
-    //                 } else {
-    //
-    //                     detectApiVersion(apiUrl).then(
-    //
-    //                         function(apiVersion) {
-    //                             resolve(apiVersion);
-    //                         });
-    //                 }
-    //             });
-    //         }
-    //     }
-    // ]);
+    .service('labelMapperSrvc', ['$q', '$http', 'settingsService',
+        function($q, $http, settingsService) {
+
+            var findUrls = function(text) {
+                var source = (text || '').toString();
+                var urlArray = [];
+                var url;
+                var matchArray;
+                var regexToken = /(((ftp|https?):\/\/)[\-\w@:%_\+.~#?&\/\/=]+)/g;
+                while ((matchArray = regexToken.exec(source)) !== null) {
+                    var token = matchArray[0];
+                    urlArray.push(token);
+                }
+                return urlArray;
+            };
+
+            this.getMappedLabel = function(label) {
+                return $q(function(resolve, reject) {
+                    var urls = findUrls(label);
+                    if (urls.length > 0) {
+                        var request = [];
+                        urls.forEach((url) => {
+                            request.push($http.get(settingsService.proxyUrl + url, {
+                                cache: true
+                            }).then((response) => {
+                                var xml = jQuery.parseXML(response.data);
+                                label = label.replace(url, $(xml).find('prefLabel').text());
+                            }, (error) => {}));
+                        });
+                        $q.all(request).then(() => {
+                            resolve(label);
+                        });
+                    } else {
+                        resolve(label);
+                    }
+                });
+            };
+        }
+    ]);
