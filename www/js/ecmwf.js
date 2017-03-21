c4343d9e52190ba5e73b4fa7b6255edbd6d3e2bd
Jan Schulte <j.schulte@52north.org>
2017-03-21 10:57:36 +0100

    support ecmwf with parent procedures and aggregated datasets

---
+++ www/js/ecmwf.js
@@ -0,0 +1,243 @@
+angular.module('n52.core.map')
+    .service('ecmwfPlatformPresenter', ['$uibModal', 'mapService',
+        function($uibModal, mapService) {
+            this.presentPlatform = function(platform) {
+                $uibModal.open({
+                    animation: true,
+                    templateUrl: 'templates/ecmwf/stationary-insitu-ecmwf.html',
+                    resolve: {
+                        selection: function() {
+                            var url = platform.url;
+                            var phenomenonId;
+                            if (mapService.map.selectedPhenomenon) {
+                                angular.forEach(mapService.map.selectedPhenomenon.provider, function(provider) {
+                                    if (url === provider.url)
+                                        phenomenonId = provider.phenomenonID;
+                                });
+                            }
+                            return {
+                                id: platform.id,
+                                phenomenonId: phenomenonId,
+                                url: url
+                            };
+                        }
+                    },
+                    controller: 'SwcModalEcmwfCtrl'
+                });
+            };
+        }
+    ])
+    .controller('SwcModalEcmwfCtrl', [
+        '$scope',
+        '$uibModalInstance',
+        'selection',
+        'seriesApiInterface',
+        'serviceFinder',
+        function(
+            $scope,
+            $uibModalInstance,
+            selection,
+            seriesApiInterface,
+            serviceFinder
+        ) {
+            $scope.serviceUrl = selection.url;
+
+            seriesApiInterface.getPlatforms(selection.id, selection.url)
+                .then((platform) => {
+                    $scope.platform = platform;
+                    seriesApiInterface.getPlatformExtras(selection.id, selection.url, {
+                            fields: 'parents'
+                        })
+                        .then((result) => {
+                            $scope.parentProcedures = result.parents.procedures;
+                        });
+                    if (platform.datasets.length > 0) {
+                        seriesApiInterface.getDatasetExtras(platform.datasets[0].id, selection.url, {
+                                fields: 'resultTimes'
+                            })
+                            .then((result) => {
+                                $scope.resultTimes = result.resultTimes;
+                            });
+                    }
+                });
+
+            $scope.close = function() {
+                $uibModalInstance.close();
+            };
+
+            $scope.selectProcedure = function(item) {
+                $scope.selectedProcedure = item;
+            };
+
+            $scope.selectResultTime = function(item) {
+                $scope.selectedResultTime = item;
+                $scope.datasets = [];
+                seriesApiInterface.getDatasets(null, selection.url, {
+                    platforms: selection.id,
+                    procedures: $scope.selectedProcedure.id
+                }).then((datasets) => {
+                    $scope.datasets = datasets;
+                    $scope.datasets.forEach(entry => {
+                        seriesApiInterface.getDatasets(entry.id, selection.url, {
+                                resultTime: $scope.selectedResultTime
+                            })
+                            .then(dataset => {
+                                dataset.selected = true;
+                                angular.extend(entry, dataset);
+                            });
+                    });
+                });
+            };
+
+            $scope.toggled = function() {
+                var allSelected = true;
+                angular.forEach($scope.platform.datasets, function(dataset) {
+                    if (!dataset.selected)
+                        allSelected = false;
+                });
+                $scope.isAllSelected = allSelected;
+            };
+
+            $scope.presentSelection = function() {
+                angular.forEach($scope.datasets, (dataset) => {
+                    dataset.filter = {
+                        resultTime: $scope.selectedResultTime
+                    };
+                    if (dataset.selected && (!selection.phenomenonId || dataset.seriesParameters.phenomenon.id === selection.phenomenonId)) {
+                        serviceFinder
+                            .getDatasetPresenter(dataset.datasetType, dataset.seriesParameters.platform.platformType, selection.url)
+                            .presentDataset(dataset, selection.url);
+                    }
+                });
+                $scope.close();
+            };
+        }
+    ])
+    .component('swcEcmwfProcedureSelection', {
+        bindings: {
+            items: '<',
+            onSelect: '&'
+        },
+        templateUrl: 'n52.ecmwf.map.procedure-selection',
+        controller: ['seriesApiInterface', 'utils',
+            function(seriesApiInterface, utils) {
+                this.$onInit = function() {};
+
+                this.onChange = function(item) {
+                    this.onSelect({
+                        item: this.selection
+                    });
+                };
+            }
+        ]
+    })
+    .component('swcEcmwfResultTimeSelection', {
+        bindings: {
+            items: '<',
+            onSelect: '&'
+        },
+        templateUrl: 'n52.ecmwf.map.result-time-selection',
+        controller: [
+            function() {
+                this.onChange = function(item) {
+                    this.onSelect({
+                        item: this.selection
+                    });
+                };
+            }
+        ]
+    })
+    .component('swcEcmwfLegend', {
+        bindings: {
+            items: '<'
+        },
+        templateUrl: 'n52.ecmwf.legend',
+        controller: ['timeseriesService', 'seriesApiInterface',
+            function(timeseriesService, seriesApiInterface) {
+
+                this.$doCheck = function() {
+                    if (Object.keys(this.items).length !== this.previousCount) {
+                        this.createEntries();
+                        this.previousCount = Object.keys(this.items).length;
+                    }
+                };
+
+                this.createEntries = function() {
+                    this.entries = [];
+                    for (var key in this.items) {
+                        if (this.items.hasOwnProperty(key)) {
+                            if (this.items[key].apiUrl.startsWith('http://colabis.dev.52north.org/ecmwf-webapp/api/v1/')) {
+                                var platformID = this.items[key].seriesParameters.platform.id;
+                                this.addToEntries(this.items[key], platformID);
+                            } else {
+                                this.entries.push(this.items[key]);
+                            }
+                        }
+                    }
+                };
+
+                this.addToEntries = function(item, platformID) {
+                  seriesApiInterface.getProcedures(item.seriesParameters.procedure.id, item.apiUrl)
+                      .then((procedure) => {
+                          var parentProcedureLabel = procedure.parents[0].label;
+                          // find entry
+                          var added = false;
+                          this.entries.forEach((entry) => {
+                              if (entry.ecmwfGroup &&
+                                  entry.platformID === platformID &&
+                                  entry.parentProcedureLabel === parentProcedureLabel) {
+                                  entry.items.push(item);
+                                  added = true;
+                              }
+                          });
+                          // create new entry
+                          if (!added) {
+                              this.entries.push({
+                                  ecmwfGroup: true,
+                                  platformID: platformID,
+                                  parentProcedureLabel: parentProcedureLabel,
+                                  items: [item]
+                              });
+                          }
+                      });
+                };
+            }
+        ]
+    })
+    .component('swcEcmwfLegendEntry', {
+        bindings: {
+            item: '<'
+        },
+        templateUrl: 'n52.ecmwf.legend-entry',
+        controller: ['styleService', 'timeseriesService', 'locateStationService', '$location',
+            function(styleService, timeseriesService, locateStationService, $location) {
+
+                this.toggleSelection = function() {
+                    this.item.items.forEach((entry) => {
+                        styleService.toggleSelection(entry);
+                    });
+                };
+
+                this.toggleVisibility = function() {
+                    this.item.items.forEach((entry) => {
+                        styleService.toggleTimeseriesVisibility(entry);
+                    });
+                };
+
+                this.removeAll = function() {
+                    this.item.items.forEach((entry) => {
+                        timeseriesService.removeTimeseries(entry.internalId);
+                    });
+                };
+
+                this.showInMap = function() {
+                    locateStationService.showPlatform('mapService', this.item.items[0]);
+                    $location.url('/map');
+                };
+
+                this.toggled = function(entry) {
+                    styleService.triggerStyleUpdate(entry);
+                };
+            }
+        ]
+    });
