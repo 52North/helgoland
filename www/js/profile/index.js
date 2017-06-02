ce0ea8bc3bb0cae2acdabfd3153ed9b8884c65f8
Jan Schulte <j.schulte@52north.org>
2017-06-02 11:44:52 +0200

    remove unused stuff

--- www/js/profile/index.js
+++ www/js/profile/index.js
@@ -3,7 +3,6 @@ export * from './services/profiles';
 export * from './view/profileChart';
 export * from './view/profileLegend';
 export * from './selection/profile-selector-view.component';
-// export * from './mockService';
 export * from './modalStationCtrl';
 export * from './overridedServices';
 export * from './timeSelector';
