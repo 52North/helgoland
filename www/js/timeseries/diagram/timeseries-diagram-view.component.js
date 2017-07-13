8b9407eac90213a726eaf6e3f50b1794677d2fb5
Jan Schulte <j.schulte@52north.org>
2017-07-13 21:27:47 +0200

    add optional time paramter to permalink generation

--- www/js/timeseries/diagram/timeseries-diagram-view.component.js
+++ www/js/timeseries/diagram/timeseries-diagram-view.component.js
@@ -18,8 +18,36 @@ angular.module('n52.core.timeseries')
             }
         ]
     })
-    .service('timeseriesDiagramPermalinkSrvc', ['timeseriesService', 'timeService', '$location', '$state',
-        function(timeseriesService, timeService, $location, $state) {
+    .component('swcDiagramPermalinkButton', {
+        bindings: {
+            generatedUrlFunction: '<'
+        },
+        templateUrl: 'n52.core.permalink.button',
+        controller: ['permalinkOpener', '$uibModal',
+            function(permalinkOpener, $uibModal) {
+                this.permalink = () => {
+                    $uibModal.open({
+                        animation: true,
+                        template: require('../../../templates/timeseries/timeseries-diagram-permalink-window.html'),
+                        controller: ['$scope', '$uibModalInstance', 'timeseriesDiagramPermalinkSrvc',
+                            function ($scope, $uibModalInstance, timeseriesDiagramPermalinkSrvc) {
+                                $scope.useTime = true;
+
+                                $scope.$watch('useTime', function() {
+                                    $scope.url = timeseriesDiagramPermalinkSrvc.createPermalink($scope.useTime);
+                                }, true);
+
+                                $scope.close = () => {
+                                    $uibModalInstance.close();
+                                };
+                            }]
+                    });
+                };
+            }
+        ]
+    })
+    .service('timeseriesDiagramPermalinkSrvc', ['timeseriesService', 'timeService', '$location', '$state', '$q', 'seriesApiInterface',
+        function(timeseriesService, timeService, $location, $state, $q, seriesApiInterface) {
             var seriesParam = 'series';
             var timeParam = 'time';
             var paramSeparator = '|';
@@ -48,25 +76,63 @@ angular.module('n52.core.timeseries')
             };
 
             this.validatePermalink = () => {
-                if($location.search()[timeParam]) {
+                var timeset = false;
+                if ($location.search()[timeParam]) {
                     var timespan = $location.search()[timeParam].split(paramSeparator);
                     if (timespan.length === 2) {
                         var start = moment(parseInt(timespan[0]));
                         var end = moment(parseInt(timespan[1]));
                         timeService.setFlexibleTimeExtent(start, end);
                     }
+                    timeset = true;
                 }
-                if($location.search()[seriesParam]) {
+                if ($location.search()[seriesParam]) {
                     timeseriesService.removeAllTimeseries();
                     var params = $location.search()[seriesParam].split(paramBlockSeparator);
-                    params.forEach(entry => {
-                        var seriesParam = entry.split(paramSeparator);
-                        if (seriesParam.length === 2) {
-                            var id = seriesParam[1];
-                            var url = seriesParam[0];
-                            timeseriesService.addTimeseriesById(id, url);
-                        }
-                    });
+                    if (timeset) {
+                        params.forEach(entry => {
+                            var seriesParam = entry.split(paramSeparator);
+                            if (seriesParam.length === 2) {
+                                var id = seriesParam[1];
+                                var url = seriesParam[0];
+                                timeseriesService.addTimeseriesById(id, url);
+                            }
+                        });
+                    } else {
+
+                        var promises = [];
+                        end = null;
+
+                        params.forEach(entry => {
+                            var seriesParam = entry.split(paramSeparator);
+                            if (seriesParam.length === 2) {
+                                var id = seriesParam[1];
+                                var url = seriesParam[0];
+                                promises.push(seriesApiInterface.getTimeseries(id, url).then(
+                                    (result) => {
+                                        if (result.lastValue && result.lastValue.timestamp) {
+                                            if (end == null || end.isBefore(moment(result.lastValue.timestamp))) {
+                                                end = moment(result.lastValue.timestamp);
+                                            }
+                                        }
+                                    }
+                                ));
+                            }
+                        });
+
+                        $q.all(promises).then(() => {
+                            var start = moment(end).subtract(2, 'days');
+                            timeService.setFlexibleTimeExtent(start, end);
+                            params.forEach(entry => {
+                                var seriesParam = entry.split(paramSeparator);
+                                if (seriesParam.length === 2) {
+                                    var id = seriesParam[1];
+                                    var url = seriesParam[0];
+                                    timeseriesService.addTimeseriesById(id, url);
+                                }
+                            });
+                        });
+                    }
                 }
             };
         }
