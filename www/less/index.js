36ccd820b2dd2ad7a1b4018fa38e53f04d9d82dd
Jan Schulte <j.schulte@52north.org>
2017-07-20 11:59:57 +0200

    update styles

--- www/less/index.js
+++ www/less/index.js
@@ -29,6 +29,7 @@ require('./map/search-control.less');
 require('./map/station-window.less');
 require('./map/zoom-control.less');
 require('./metadata/procedure.less');
+require('./selection/main.less');
 require('./selection/multi-service-filter-selector.less');
 require('./selection/service-filter-selection.less');
 require('./selectionList/selection-list.less');
