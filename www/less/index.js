8de63a2efb5f4f11f56f04bfdf4fb4887ffb8b7e
Jan Schulte <j.schulte@52north.org>
2017-07-21 12:09:00 +0200

    update selector-entry styles

--- www/less/index.js
+++ www/less/index.js
@@ -32,7 +32,6 @@ require('./map/zoom-control.less');
 require('./metadata/procedure.less');
 require('./selection/main.less');
 require('./selection/multi-service-filter-selector.less');
-require('./selection/service-filter-selection.less');
 require('./selectionList/selection-list.less');
 require('./settings/settings-window.less');
 require('./table/table.less');
