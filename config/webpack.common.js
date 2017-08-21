e612f8184e5ced7018fabfdd6858532e988d3ec3
Jan Schulte <j.schulte@52north.org>
2017-08-21 13:43:06 +0200

    Update bootstrap

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -46,11 +46,11 @@ module.exports = function(options) {
             }, {
                 test: /\.less$/,
                 use: [{
-                    loader: "style-loader" // creates style nodes from JS strings
+                    loader: 'style-loader' // creates style nodes from JS strings
                 }, {
-                    loader: "css-loader" // translates CSS into CommonJS
+                    loader: 'css-loader' // translates CSS into CommonJS
                 }, {
-                    loader: "less-loader" // compiles Less to CSS
+                    loader: 'less-loader' // compiles Less to CSS
                 }]
             }, {
                 test: /\.scss$/,
@@ -92,19 +92,18 @@ module.exports = function(options) {
                 $: 'jquery',
                 jquery: 'jquery',
                 'window.jQuery': 'jquery',
-                Tether: "tether",
-                "window.Tether": "tether",
-                Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
-                Button: "exports-loader?Button!bootstrap/js/dist/button",
-                Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
-                Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
-                Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
-                Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
-                Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
-                Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
-                Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
-                Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
-                Util: "exports-loader?Util!bootstrap/js/dist/util",
+                Popper: 'popper.js',
+                Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+                Button: 'exports-loader?Button!bootstrap/js/dist/button',
+                Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+                Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+                Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+                Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+                Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+                Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+                Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+                Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
+                Util: 'exports-loader?Util!bootstrap/js/dist/util',
             }),
             new HtmlWebpackPlugin({
                 template: './www/index.html',
