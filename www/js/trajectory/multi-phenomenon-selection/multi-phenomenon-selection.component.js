7cbb59f64d3362cf611b97b1543bbd57c403d6fd
Jan Schulte <j.schulte@52north.org>
2017-09-27 09:55:52 +0200

    move multi phenomenon selection to view and add color support

---
+++ www/js/trajectory/multi-phenomenon-selection/multi-phenomenon-selection.component.js
@@ -0,0 +1,42 @@
+import 'n52-sensorweb-client-core/src/js/selection/provider-selector/component';
+import 'n52-sensorweb-client-core/src/js/selection/multi-service-filter-selector/component';
+
+angular.module('n52.core.trajectory')
+    .component('swcMultiPhenomenonButton', {
+        bindings: {
+            dataset: '<',
+            feature: '<',
+            selectedPhenomena: '<',
+            selectedPhenomenaChanged: "&onSelectedPhenomenaChanged",
+        },
+        template: require('./multi-phenomenon-selection.component.html'),
+        controller: ['seriesApiInterface', 'colorService',
+            function(seriesApiInterface, colorService) {
+
+                this.$onChanges = (changes) => {
+                    if (changes.feature && this.feature) {
+                        seriesApiInterface.getPhenomena(null, this.dataset.providerUrl, {
+                            platformTypes: 'mobile',
+                            features: this.feature.id
+                        }).then(result => {
+                            this.visible = result.length > 1;
+                            this.phenomenaList = result.filter((entry) => {
+                                entry.color = colorService.getColor(entry.id);
+                                if (entry.id !== this.dataset.seriesParameters.phenomenon.id) {
+                                    return true;
+                                }
+                            });
+                        });
+                    }
+                };
+
+                this.toggleSelection = (entry) => {
+                    entry.selected = !entry.selected;
+                    this.selectedPhenomenaChanged({
+                        phenomenaList: this.phenomenaList
+                    });
+                };
+
+            }
+        ]
+    });
