7cb2eb0440c18048aa1fe120ca26e61571b1f8c0
Jan Schulte <j.schulte@52north.org>
2017-10-25 13:03:34 +0200

    fix problem with leaflet marker pngs

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -70,3 +70,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Need to import at least one locale-data with intl.
  */
 // import 'intl/locale-data/jsonp/en';
+
+import * as L from 'leaflet';
+L.Icon.Default.imagePath = 'assets/';
