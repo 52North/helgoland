a282c9af3bfa6d89389166a8a28dcb7fd267872e
Jan Schulte <j.schulte@52north.org>
2017-11-14 12:01:08 +0100

    adjust trajectory permalink

--- src/app/trajectories/view/view-permalink.ts
+++ src/app/trajectories/view/view-permalink.ts
@@ -1,32 +1,40 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PermalinkService } from 'helgoland-toolbox';
+import { ColorService, DatasetOptions, PermalinkService } from 'helgoland-toolbox';
 
 import { TrajectoriesService } from './../services/trajectories.service';
 
-const PARAM_ID = 'id';
+const PARAM_IDS = 'id';
+const PARAM_ID_SEPERATOR = '|';
 
 @Injectable()
 export class TrajectoriesViewPermalink extends PermalinkService<void> {
 
     constructor(
         private trajectories: TrajectoriesService,
-        private activatedRoute: ActivatedRoute
+        private activatedRoute: ActivatedRoute,
+        private color: ColorService
     ) {
         super();
     }
 
     public validatePeramlink(): void {
         this.activatedRoute.queryParamMap.subscribe(map => {
-            if (map.has(PARAM_ID)) {
-                this.trajectories.addDataset(map.get(PARAM_ID));
+            if (map.has(PARAM_IDS)) {
+                const ids = map.get(PARAM_IDS).split(PARAM_ID_SEPERATOR);
+                this.trajectories.removeAllDatasets();
+                ids.forEach(entry => {
+                    const option = new DatasetOptions(entry, this.color.getColor());
+                    option.visible = false;
+                    this.trajectories.addDataset(entry, option);
+                });
             }
         });
     }
 
     protected generatePermalink(): string {
         if (this.trajectories.hasDatasets()) {
-            return this.createBaseUrl() + '?' + PARAM_ID + '=' + encodeURIComponent(this.trajectories.datasetIds[0]);
+            return this.createBaseUrl() + '?' + PARAM_IDS + '=' + encodeURIComponent(this.trajectories.datasetIds.join(PARAM_ID_SEPERATOR));
         }
         return '';
     }
