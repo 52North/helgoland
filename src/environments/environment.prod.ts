79766e2a2f82162c79e67e8030d051988be491bf
Jan Schulte <j.schulte@52north.org>
2018-01-16 13:04:45 +0100

    adjust settings loading

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
+import { Settings } from 'helgoland-toolbox';
+
 export const environment = {
   production: true
 };
+
+export let settings: Settings;
+
+export const settingsPromise = new Promise<Settings>((resolve, reject) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', './assets/settings.json');
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      settings = JSON.parse(xhr.responseText);
+      resolve(settings);
+    } else {
+      reject('Cannot load configuration');
+    }
+  };
+  xhr.send();
+});
