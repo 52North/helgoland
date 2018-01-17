7ddc301a1981c9aadcb88c043cd552281bbef8ee
Jan Schulte <j.schulte@52north.org>
2018-01-17 09:26:57 +0100

    adjust permalink buttons

---
+++ src/app/components/permalink/in-new-window/in-new-window.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { PermalinkNewWindowComponent } from 'helgoland-toolbox';
+
+@Component({
+  selector: 'n52-custom-permalink-new-window',
+  templateUrl: './in-new-window.component.html',
+  styleUrls: ['./in-new-window.component.scss']
+})
+export class InNewWindowComponent extends PermalinkNewWindowComponent { }
