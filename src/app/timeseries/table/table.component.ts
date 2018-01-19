f53ebd5931de2796e2eb7524b28c1312952892a0
Christoph Friedrich <christoph@friedrich-whv.de>
2018-01-19 15:06:30 +0100

    add legend to table view
    analogous to diagram view (although meanwhile the diagram view has a
    more responsive version)

--- src/app/timeseries/table/table.component.ts
+++ src/app/timeseries/table/table.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TimeseriesService } from '../services/timeseries.service';
-import { Timespan, DatasetOptions } from 'helgoland-toolbox';
+import { Timespan, DatasetOptions, Time } from 'helgoland-toolbox';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  ModalTimeseriesTimespanComponent,
+} from '../../components/modal-timeseries-timespan/modal-timeseries-timespan.component';
+import { ModalOptionsEditorComponent } from '../../components/modal-options-editor/modal-options-editor.component';
+import { ModalGeometryViewerComponent } from '../../components/modal-geometry-viewer/modal-geometry-viewer.component';
 
 @Component({
   selector: 'n52-table',
@@ -17,12 +23,60 @@ export class TimeseriesTableComponent implements OnInit {
 
   constructor(
     private timeseriesService: TimeseriesService,
+    private timeSrvc: Time,
+    private modalService: NgbModal,
   ) { }
 
   public ngOnInit() {
     this.datasetIds = this.timeseriesService.datasetIds;
     this.timespan = this.timeseriesService.timespan;
     this.datasetOptions = this.timeseriesService.datasetOptions;
+  }
+
+  public selectTimeseries(selected: boolean, internalId: string) {
+    if (selected) {
+        this.selectedIds.push(internalId);
+    } else {
+        this.selectedIds.splice(this.selectedIds.findIndex(entry => entry === internalId), 1);
+    }
+  }
+
+  public isSelected(internalId: string) {
+    return this.selectedIds.find(e => e === internalId);
+  }
+
+  public timeChanged(timespan: Timespan) {
+    this.updateTime(timespan);
+  }
+
+  public jumpToDate(date: Date) {
+      this.updateTime(this.timeSrvc.centerTimespan(this.timespan, date));
+  }
+
+  public openTimeSettings() {
+    const ref = this.modalService.open(ModalTimeseriesTimespanComponent);
+    (ref.componentInstance as ModalTimeseriesTimespanComponent).timespan = this.timespan;
+    (ref.componentInstance as ModalTimeseriesTimespanComponent).onTimespanChanged
+        .subscribe((timespan: Timespan) => this.updateTime(timespan));
+  }
+
+  private updateTime(timespan: Timespan) {
+    this.timeseriesService.setTimespan(timespan);
+    this.timespan = timespan;
+  }
+
+  public updateOptions(options: DatasetOptions, internalId: string) {
+    this.timeseriesService.updateDatasetOptions(options, internalId);
+  }
+
+  public editOption(options: DatasetOptions) {
+    const ref = this.modalService.open(ModalOptionsEditorComponent);
+    (ref.componentInstance as ModalOptionsEditorComponent).options = options;
+  }
+
+  public showGeometry(geometry: GeoJSON.GeoJsonObject) {
+      const ref = this.modalService.open(ModalGeometryViewerComponent);
+      (ref.componentInstance as ModalGeometryViewerComponent).geometry = geometry;
   }
 
 }
