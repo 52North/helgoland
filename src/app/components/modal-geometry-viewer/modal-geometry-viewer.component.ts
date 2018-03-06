1c9dda86d0d7d166f8b50773d536533c9ac827bd
Jan Schulte <j.schulte@52north.org>
2018-03-06 16:33:26 +0100

    fix map issue in modal windows

--- src/app/components/modal-geometry-viewer/modal-geometry-viewer.component.ts
+++ src/app/components/modal-geometry-viewer/modal-geometry-viewer.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
+import { MapCache } from '@helgoland/map';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -6,13 +7,20 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './modal-geometry-viewer.component.html',
   styleUrls: ['./modal-geometry-viewer.component.scss']
 })
-export class ModalGeometryViewerComponent {
+export class ModalGeometryViewerComponent implements AfterViewInit {
+
+  public mapId = 'mapGeometryViewerModal';
 
   @Input()
   public geometry: GeoJSON.GeoJsonObject;
 
   constructor(
-    public activeModal: NgbActiveModal
+    public activeModal: NgbActiveModal,
+    public mapCache: MapCache
   ) { }
+
+  public ngAfterViewInit(): void {
+    window.setTimeout(() => this.mapCache.getMap(this.mapId).invalidateSize(), 10);
+  }
 
 }
