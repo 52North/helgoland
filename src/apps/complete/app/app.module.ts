22e7626b8188fbc4ded2018cf1cfbacd438e5dfe
Jan Schulte <j.schulte@52north.org>
2019-06-25 17:36:17 +0200

    adjust local selector

--- src/apps/complete/app/app.module.ts
+++ src/apps/complete/app/app.module.ts
@@ -6,14 +6,7 @@ import { Routes } from '@angular/router/src/config';
 import { HelgolandCachingModule } from '@helgoland/caching';
 import { DatasetApiInterface, Settings, SettingsService, SplittedDataDatasetApiInterface } from '@helgoland/core';
 import { JsonFavoriteExporterService } from '@helgoland/favorite';
-import {
-  NgbAccordionModule,
-  NgbDatepickerModule,
-  NgbDropdownModule,
-  NgbModalModule,
-  NgbTabsetModule,
-  NgbTimepickerModule,
-} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
@@ -66,12 +59,7 @@ export class ExtendedSettingsService extends SettingsService<Settings> {
     InfoModule,
     HttpClientModule,
     HelgolandCachingModule,
-    NgbTabsetModule.forRoot(),
-    NgbAccordionModule.forRoot(),
-    NgbModalModule.forRoot(),
-    NgbDropdownModule.forRoot(),
-    NgbDatepickerModule.forRoot(),
-    NgbTimepickerModule.forRoot(),
+    NgbModule,
     RouterModule.forRoot(
       baseRoutes
     )
