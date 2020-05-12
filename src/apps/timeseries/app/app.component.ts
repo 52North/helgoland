88fad0fd01780c88e91845624206f081eb842dc9
Jan Schulte <j.schulte@52north.org>
2020-05-12 15:54:15 +0200

    remove unused d3 translation

--- src/apps/timeseries/app/app.component.ts
+++ src/apps/timeseries/app/app.component.ts
@@ -2,7 +2,6 @@ import { registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
 import { Component, ViewEncapsulation } from '@angular/core';
 import { Language, Settings, SettingsService } from '@helgoland/core';
-import { D3TimeFormatLocaleService } from '@helgoland/d3';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -18,7 +17,6 @@ export class AppComponent {
   constructor(
     private translate: TranslateService,
     private settings: SettingsService<Settings>,
-    private d3translate: D3TimeFormatLocaleService
   ) {
     this.translate.setDefaultLang('en');
     this.translate.use('de');
@@ -27,18 +25,5 @@ export class AppComponent {
     registerLocaleData(localeDe);
 
     this.languageList = this.settings.getSettings().languages;
-
-    this.d3translate.addTimeFormatLocale('de',
-      {
-        'dateTime': '%a %b %e %X %Y',
-        'date': '%d-%m-%Y',
-        'time': '%H:%M:%S',
-        'periods': ['AM', 'PM'],
-        'days': ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
-        'shortDays': ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
-        'months': ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
-        'shortMonths': ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez']
-      }
-    );
   }
 }
