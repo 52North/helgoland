b29b35b4d9b32333c7b21cf3e1d142b289b69f66
Jan Schulte <j.schulte@52north.org>
2020-09-23 14:20:22 +0200

    disable adjust script

--- scripts/adjust-install.ts
+++ scripts/adjust-install.ts
@@ -1,11 +1,11 @@
-import { readFile, writeFile } from 'fs';
+// import { readFile, writeFile } from 'fs';
 
-const fileToD3 = 'node_modules/plotly.js/node_modules/d3/d3.js';
-// temporary fix for the following issue: https://github.com/plotly/plotly.js/issues/3518
-readFile(fileToD3, 'utf8', function (err, obj) {
-    // find and replace fragment
-    const replaced = obj.replace(/}\(\);$/, '}.apply(self);');
-    writeFile(fileToD3, replaced, (error) => {
-        if (error) { console.error(error); }
-    });
-});
+// const fileToD3 = 'node_modules/plotly.js/node_modules/d3/d3.js';
+// // temporary fix for the following issue: https://github.com/plotly/plotly.js/issues/3518
+// readFile(fileToD3, 'utf8', function (err, obj) {
+//     // find and replace fragment
+//     const replaced = obj.replace(/}\(\);$/, '}.apply(self);');
+//     writeFile(fileToD3, replaced, (error) => {
+//         if (error) { console.error(error); }
+//     });
+// });
