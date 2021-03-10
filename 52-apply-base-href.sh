743907f3c59facbc43c1ff80ffeece3107f94cad
EHJ-52n <e.h.juerrens@52north.org>
2021-03-10 14:35:52 +0100

    Fix nginx try_files not supporting context path change

--- 52-apply-base-href.sh
+++ 52-apply-base-href.sh
@@ -5,24 +5,24 @@
 #
 set -e
 DEFAULT_BASE_HREF=/
-if [ -z "${BASE_HREF}" ]; then
-   echo "ENV BASE_HREF not set. Using default: '$DEFAULT_BASE_HREF'"
+LOCK_FILE=/base_href_applied
+if [ -f "${LOCK_FILE}" ]; then
+    echo "Base href already applied. Updates are not possible"
+    exit 0
+fi
+if [ -z "${BASE_HREF}" ] || [ "${BASE_HREF}" = "${DEFAULT_BASE_HREF}" ]; then
+   echo "Using default base href: '$DEFAULT_BASE_HREF'"
+   BASE_HREF=$DEFAULT_BASE_HREF
+   sed --in-place "s@BASE_HREF@${BASE_HREF}@g" "/etc/nginx/conf.d/default.conf"
    exit 0
 fi
-if [ "${BASE_HREF}" = "${DEFAULT_BASE_HREF}" ]; then
-    echo "Base Href is default '${BASE_HREF}'"
-    exit 0
-fi
-if [ -d "/usr/share/nginx/html${BASE_HREF}" ]; then
-    echo "Base href '${BASE_HREF}' already applied: '/usr/share/nginx/html${BASE_HREF}'"
-    exit 0
-fi
-echo "Base Href is not default and not applied"
+echo "Base Href '${BASE_HREF}' is not default and not applied"
 # TODO
 # check for "/" at beginning and end
-echo "Using configured base href: '${BASE_HREF}'"
 mv --verbose /usr/share/nginx/html /tmp/helgo
 mkdir --verbose --parents /usr/share/nginx/html
 mv --verbose /tmp/helgo "/usr/share/nginx/html${BASE_HREF}"
 sed --in-place "s@base href=\"/\"@base href=\"${BASE_HREF}\"@g" "/usr/share/nginx/html${BASE_HREF}index.html"
+sed --in-place "s@BASE_HREF@${BASE_HREF}@g" "/etc/nginx/conf.d/default.conf"
+touch ${LOCK_FILE}
 echo "Base href application done"
